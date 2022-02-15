fe0ef5c4cd2e1d109abe44d90da2275ea89ba91a
yutest1 <ito@texia.co.jp>
2022-02-15 16:14:37 +0900

    phpテスト１新規追加

---
+++ action.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>test1</title>
+  </head>
+  <body>
+    <h1>名前と生まれた月</h1>
+    こんにちは！<?php echo $_POST['name']?>さん
+    <br>
+    あなたの生まれた月は<?php echo $_POST['month']?>月です。
+  </body>
+</html>
---
+++ test1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>test1</title>
+  </head>
+  <body>
+    <h1>入力画面</h1>
+    <form action="action.php" method="post"> 
+        名前:<input type="text" name="name"/>
+        生まれた月:<input type="text" name="month"/>
+        <input type="submit">
+    </form>
+  </body>
+</html>
