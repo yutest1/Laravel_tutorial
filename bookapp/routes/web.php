de5014b8f0ae8f90c33c0ee20ec555f51908bf0d
yutest1 <ito@texia.co.jp>
2022-02-17 18:05:59 +0900

    書籍管理アプリ

---
+++ bookapp/routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Book;
+use Illuminate\Http\Request;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $books = Book::all();
+    return view('books',['books' => $books]);
+})->middleware('auth');
+
+Route::post ('/book', function(Request $request){
+    $validator = Validator::make($request->all(),[
+        'name' => 'required|max:255',
+    ]);
+    $book = new Book;
+    $book->title = $request->name;
+    $book ->save();
+
+    return redirect('/');
+});
+
+Route::delete('/book/{book}',function(Book $book){
+    $book -> delete();
+
+    return redirect('/');
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
