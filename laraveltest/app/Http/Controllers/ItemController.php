d187318b0a1da2448cb84159d5d91c418d88eabc
yutest1 <ito@texia.co.jp>
2022-02-22 11:12:54 +0900

    編集

--- laraveltest/app/Http/Controllers/ItemController.php
+++ laraveltest/app/Http/Controllers/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Item;
 use Illuminate\Http\Request; //Request.phpに定義されているメソッドが使えるようになる
 use Illuminate\Http\Response;
+use App\Http\Requests\ItemRequest;
 
 class ItemController extends Controller //定義したクラス名ItemControllerに、親クラスとして、Controllerクラスを継承している。Controllerクラスには、validate()やmiddleware()など便利なメソッドが定義
 {
@@ -15,10 +16,69 @@ class ItemController extends Controller //定義したクラス名ItemController
      * @return Response
      */
     public function index(Request $request){
-      $items = Item::orderBy('created_at', 'desc')->get();//降順でデータを取得
+      
+      $query = Item::query(); //Itemモデルのクエリビルダが$queryに代入
 
-      return view('items.index', [
-        'items' => $items
-      ]);
+        if(isset($request->name)){
+            $query->where('name', 'like', '%'.$request->name.'%');
+        }
+
+        if(isset($request->sex)){
+            $query->where('sex', $request->sex);
+        }
+
+        if(isset($request->memo)){
+            $query->where('memo', 'like', '%'.$request->memo.'%');
+        }
+
+        $items = $query->orderBy('created_at', 'desc')->paginate(3)->appends($request->all());; //降順でデータを取得
+
+        return view('items.index', [
+            'items' => $items
+        ]);
     }
+
+    public function create(Request $request){
+      return view('items.create');
+  }
+
+  public function store(ItemRequest $request){
+    $item = new Item();
+    $item->name = $request->name;
+    $item->age = $request->age;
+    $item->sex = $request->sex;
+    $item->memo = $request->memo;
+    $item->save();
+    return redirect()->action('ItemController@index');
+  }
+
+  public function show(string $id){
+    $item = Item::findOrFail($id);
+
+    return view('items.show')->with('item', $item);
+  }
+//編集画面
+  public function edit(String $id){
+    return view('items.edit')->with('item', Item::findOrFail($id));
+  }
+//編集処理
+  public function update(ItemRequest $request, string $id){
+    $item = Item::findOrFail($id);
+    $item->fill($request->all())->save();
+    return redirect()->route('index');
+}
+
+//削除画面
+public function delete(Item $item){
+  return view('items.delete')->with('item', $item);
+
+
+
+}
+//削除処理
+public function destroy(Item $item){
+  $item->delete();
+  return redirect()->route('index');
+}
+
 }
--- laraveltest/routes/web.php
+++ laraveltest/routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', 'ItemController@index');
+Route::get('/', 'ItemController@index')->name('index'); //一覧画面へのルート定義
+Route::get('/items/create', 'ItemController@create')->name('create'); //登録画面へのルート定義
+Route::post('/items', 'ItemController@store')->name('store'); //登録画面の保存のルート定義
+Route::get('/items/{id}', 'ItemController@show')->name('show')->where('id', '[0-9]+'); //詳細画面へのルート定義
+Route::get('/items/{id}/edit', 'ItemController@edit')->name('edit')->where('id', '[0-9]+'); //編集画面へのルート定義
+Route::patch('/items/{id}', 'ItemController@update')->name('update')->where('id', '[0-9]+'); //登録画面の保存ルート定義
+Route::get('/items/{item}/delete', 'ItemController@delete')->name('delete')->where('item', '[0-9]+'); //削除画面のルート定義
+Route::delete('/items/{item}', 'ItemController@destroy')->name('destroy')->where('item', '[0-9]+');  //削除画面の削除ルート定義
 
 
